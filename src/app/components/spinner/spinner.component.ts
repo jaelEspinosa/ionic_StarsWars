4920d78fd0f25ae3f12049e912290afc04991124
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-10-21 00:43:32 +0200

    fix styles

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -8,9 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SpinnerComponent  implements OnInit {
 
   @Input() message:string = '';
-
+  public progress:number = 0.01;
+  public buffer: number = 0.5
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
 }
