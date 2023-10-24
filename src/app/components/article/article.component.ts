5dbbd88929cfb84851dbc320b83bffb8ccb648f2
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-10-24 17:09:56 +0200

    add favorites function

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetButton, ActionSheetController } from '@ionic/angular';
 
 
 import { Data } from 'src/app/interfaces/starsWars';
@@ -15,39 +15,72 @@ export class ArticleComponent  implements OnInit {
   @Input() i!: number
 
   public imageLoaded: boolean = false;
+  public favoriteCards : Data[] = []
 
   public actionCtrl = inject ( ActionSheetController )
+
+  public buttons:  ActionSheetButton<any>[] = [];
 
   constructor() { }
 
   ngOnInit() {
+     if(localStorage.getItem('favorites')){
+       this.favoriteCards = JSON.parse(localStorage.getItem('favorites')!)
+     }
 
+     let existeItem:boolean = false
 
+    this.favoriteCards.forEach(item => {
+      if(item._id === this.character._id){
+        existeItem = true;
+      }
+    })
+   if(existeItem){
+    this.buttons = [
+      {
+        text:'Share',
+        icon: 'share-outline',
+        handler:()=>this.onShareArticle()
+      },
+      {
+        text:'Remove',
+        icon: 'heart',
+        handler:()=>this.removeFavorite()
+      },
+      {
+        text:'Cancel',
+        icon: 'close-outline',
+        role:'cancelar'
+      }
+    ]
+   }else{
+    this.buttons = [
+      {
+        text:'Share',
+        icon: 'share-outline',
+        handler:()=>this.onShareArticle()
+      },
+      {
+        text:'Favorite',
+        icon: 'heart-outline',
+        handler:()=>this.onToggleFavorite()
+      },
+      {
+        text:'Cancel',
+        icon: 'close-outline',
+        role:'cancelar'
+      }
+    ]
+   }
   }
-  async onOpenMenu(){
 
+  async onOpenMenu(){
     const actionSheet = await this.actionCtrl.create({
       header:'Opciones',
       mode:'ios',
       cssClass:'myActSheet',
 
-      buttons:[
-        {
-          text:'Compartir',
-          icon: 'share-outline',
-          handler:()=>this.onShareArticle()
-        },
-        {
-          text:'Favorito',
-          icon: 'heart-outline',
-          handler:()=>this.onToggleFavorite()
-        },
-        {
-          text:'Cancelar',
-          icon: 'close-outline',
-          role:'cancelar'
-        }
-      ]
+      buttons: this.buttons
     })
    await actionSheet.present()
   }
@@ -57,6 +90,26 @@ export class ArticleComponent  implements OnInit {
   }
 
   onToggleFavorite() {
-    console.log('Toggled Favorite')
+
+    if(localStorage.getItem('favorites')){
+      this.favoriteCards = JSON.parse(localStorage.getItem('favorites')!)
+    }
+    let existeItem:boolean = false
+
+    this.favoriteCards.forEach(item => {
+      if(item._id === this.character._id){
+        existeItem = true;
+      }
+    })
+    if (!existeItem){
+      this.favoriteCards.push(this.character)
+      localStorage.setItem('favorites',JSON.stringify(this.favoriteCards))
+      this.favoriteCards = JSON.parse(localStorage.getItem('favorites')!)
+    }
+  }
+
+  removeFavorite(){
+    const NewFavorites = this.favoriteCards.filter(item=>item._id !== this.character._id);
+    localStorage.setItem('favorites', JSON.stringify(NewFavorites))
   }
 }
