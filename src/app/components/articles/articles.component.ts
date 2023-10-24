7a8f5cd1ad96d7a90237c1b40e0f7c7ae8bd9653
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-10-24 19:14:12 +0200

    ok

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { Data } from 'src/app/interfaces/starsWars';
 
@@ -7,11 +7,15 @@ import { Data } from 'src/app/interfaces/starsWars';
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent  implements OnInit {
+export class ArticlesComponent  implements OnInit, OnChanges {
 
   @Input() characters:Data[]   = [];
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges){
+    console.log('se detectaron cambios en characters', changes)
+
+  }
 
   ngOnInit() {}
 
